aedb312caf52dc0017988f10cd74a3bee57c7385
adityaparab04 <adityaparab04@gmail.com>
2024-04-21 21:59:49 -0400

    Typescript generics

--- AdvanceTypes/generics.ts
+++ AdvanceTypes/generics.ts
@@ -1,9 +1,24 @@
 function getSecond<T>(array: T[]) {
-	return array[1];
+	return array[1]
 }
 
-const a5 = [1, 2, 3];
-const b5 = ["asd", "sdf"];
+const a5 = [1, 2, 3]
+const b5 = ["asd", "sdf"]
 
-const retA = getSecond(a5);
-const retB = getSecond(b5);
+const retA = getSecond(a5)
+const retB = getSecond(b5)
+
+type APTResponse<TData> = {
+	data: TData
+	isError: boolean
+}
+
+type UserResponse = APIResponse<{
+	name: string
+	age: number
+}>
+
+const a6: UserResponse = {
+	data: { name: "Aditya", age: 25 },
+	isError: false,
+}
