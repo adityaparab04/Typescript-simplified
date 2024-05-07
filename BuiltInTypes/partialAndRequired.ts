dc6c2a141852cb685bb7ff988ec58945306560ef
adityaparab04 <adityaparab04@gmail.com>
2024-05-06 22:49:36 -0400

    error solved

--- BuiltInTypes/partialAndRequired.ts
+++ BuiltInTypes/partialAndRequired.ts
@@ -16,4 +16,4 @@ const todo_1: FormTodo = {
 type RequiredPick<T, Key extends keyof T> = Required<Pick<T, Key>>;
 type PartialPick<T, Key extends keyof T> = Partial<Pick<T, Key>> & Omit<T, Key>;
 
-type FormTodo_1 = RequiredPick<Todo, "title">;
+type FormTodo_1 = RequiredPick<FormTodo, "title">;
